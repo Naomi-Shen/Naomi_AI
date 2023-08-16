9aa3f1d5274f3b9abe05b6ad1453021258c4e061
Naomi.Shen <862959060@qq.com>
2023-08-16 22:53:13 +0800

    update learning record

--- Pytorch/00_pytorch_fundamentals_video.py
+++ Pytorch/00_pytorch_fundamentals_video.py
@@ -120,3 +120,129 @@ ones
 ones.dtype
 
 random_tensor.dtype
+
+"""### Creating a range of tensors and tensors-like"""
+
+# Use torch.range() and get deprecated message, use torch.arange()
+one_to_ten = torch.arange(start=1,end=11,step=1)
+one_to_ten
+
+#creating tensors like
+ten_zeros = torch.zeros_like(input=one_to_ten)
+ten_zeros
+
+"""### Tensor datatypes
+
+**Note:** Tensor datatypes is one of the 3 big issues/errors with PyTorch& Deep learning
+1. Tensor not right datatype
+2. Tensor not right shape
+3. Tensor not on the right device
+
+Precision in computing-
+https://en.wikipedia.org/wiki/Precision_(computer_science)
+
+"""
+
+# Float 32 tensor
+float_32_tensor = torch.tensor([3.0,6.0,9.0],
+                dtype=None,# What datatype is the tensor,(e.g. float32 or float16)可以被定义为torch.float16等等；matter with precision in computing
+                device="cuda",# What device is your tensor on
+                requires_grad=False)# Whether or not to track gradients with this tensors operation
+float_32_tensor
+
+float_32_tensor.dtype
+
+float_16_tensor = float_32_tensor.type(torch.float16)
+float_16_tensor # check the tensor datatype
+
+float_16_tensor*float_32_tensor#此处没有出现error，但其他dtype不match的情况可能会error
+
+int_32_tensor = torch.tensor([3,6,9],dtype=torch.int32)# no dots to make it float
+int_32_tensor
+
+float_32_tensor * int_32_tensor
+
+"""### Getting information from tensors (tensor attributes)
+
+1. Tensor not right datatype - to get datatype from a tensor, can use'tensor.dtype'
+2. Tensor not right shape - to get shape from a tensor, can use 'tensor.shape'
+3. Tensor not on the right device - to get device from a tensor, can use 'tensor.device'
+"""
+
+# create a tensor
+some_tensor = torch.rand(3,4)
+some_tensor
+
+some_tensor.size, some_tensor.shape #attribute doesn't have a bracket
+
+some_tensor.size
+
+some_tensor.size() #function has its bracket behind
+
+# Find out details about some tensor
+print(some_tensor)
+print(f"Datatype of tensor:{some_tensor.dtype}")
+print(f"Shape of tensor:{some_tensor.shape}")
+print(f"Device tensor is on:{some_tensor.device}")
+
+"""from ast import Add
+### Manipulating Tensors(tensor operations)
+
+Tensor operations include:
+* Addition
+* Subtraction
+* Multiplication(element-wise)
+* Division
+* Matrix multiplication
+"""
+
+# Create a tensor and add 1- to it
+tensor = torch.tensor([1,2,3])
+tensor+10
+
+# Multiply tensor by 10
+tensor*10
+
+tensor
+
+# Subtract 10
+tensor-10
+
+# Try out PyTorch in-built functions
+torch.mul(tensor,10)
+
+torch.add(tensor,10)
+
+"""### Matrix multiplication
+
+Two main ways of performing multiplication in neural networkd and deep learning
+
+1 Element-wise multiplication
+2 Matrix multiplication(dot product)
+
+More information on multiplying matrixes-
+https://www.mathsisfun.com/algebra/matrix-multiplying.html
+"""
+
+# Element wise multiplication
+print(tensor,"*",tensor)
+print(f"Equals:{tensor*tensor}")
+
+# Matrix multiplication
+torch.matmul(tensor,tensor)
+
+tensor
+
+#matrix multiplication by hand
+1*1+2*2+3*3
+
+# Commented out IPython magic to ensure Python compatibility.
+# %%time
+# value=0
+# for i in range(len(tensor)):
+#  value +=tensor[i]*tensor[i]
+# print(value)
+
+# Commented out IPython magic to ensure Python compatibility.
+# %%time
+# torch.matmul(tensor,tensor)
